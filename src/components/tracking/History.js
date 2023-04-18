63d44d6f1bc440bd7faab761f42581c886e85423
tripn11 <noblenwala.nn@gmail.com>
2023-04-18 22:33:34 +0100

    status highlighter added

--- src/components/tracking/History.js
+++ src/components/tracking/History.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 
-export default (props) => (
-    <div className='item'>
+export default (props) => {
+    const [myColor, setMyColor] = useState('black');
+
+    useEffect(()=>{
+        switch (props.details.status) {
+            case 'pending' :
+                setMyColor('red');
+                break;
+            
+            case 'on hold':
+                setMyColor('red');
+                break;
+
+            case 'in transit':
+                setMyColor('blue');
+                break;
+
+            case 'delivered':
+                setMyColor('green');
+                break;
+            default:
+                setMyColor('black');
+        }
+    }, [props.details.status])
+
+    return (<div className='item'>
         <p>{moment(props.details.editedAt).format('DD-MM-YYYY')}</p>
         <p>{props.details.location}</p>
-        <p>{props.details.status}</p>
+        <p style={{color:myColor}}>{props.details.status}</p>
         <p>{props.details.remark}</p>
     </div>
-)
+)}
